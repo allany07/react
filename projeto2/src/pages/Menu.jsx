da8f17d9abcf04e31a06af627c14ac0112f530c2
allany07 <allany01santos@gmail.com>
2025-06-04 20:29:21 -0300

    criando rotas

---
+++ projeto2/src/pages/Menu.jsx
@@ -0,0 +1,11 @@
+function Menu(){
+    return(
+        <div style={{ display: 'flex', gap: '20px', marginTop: '10px' }}>
+            <a href="/">Home</a>{" "}
+            <a href="/sobre">Sobre</a>{" "}
+            <a href="/contato">Contato</a><br />
+        </div>
+    )
+}
+
+export default Menu

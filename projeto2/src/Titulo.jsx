d759fb2c67a902d7a987ef6b1324a498570138d0
allany07 <allany01santos@gmail.com>
2025-06-04 18:02:59 -0300

    colocando javascript misturado com html e colocando url

--- projeto2/src/Titulo.jsx
+++ projeto2/src/Titulo.jsx
@@ -1,7 +1,13 @@
 function Titulo(){
+
+    let nome = "Allany Santos";
+    const soma = 10 + 11;
+    const urlImg = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png";
+
     return (
         <div>
-            <div><h1>Oi eu sou um título</h1></div>
+            <h1>Oi eu sou {nome} e tenho {soma} anos</h1>
+            <img width={150} src={urlImg}/>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci atque fuga corporis, et dolorem, mollitia eveniet distinctio ab deserunt rem aut delectus. Quasi obcaecati, omnis in harum a quae error!</p>
         </div>
     )

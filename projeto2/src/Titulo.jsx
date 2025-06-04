5e8162a702bd37ae223660e0a089f10de39270b1
allany07 <allany01santos@gmail.com>
2025-06-04 17:43:11 -0300

    praticando a estrutura de componentes, adicionando texto em arquivos e chamando no arquivo App

---
+++ projeto2/src/Titulo.jsx
@@ -0,0 +1,10 @@
+function Titulo(){
+    return (
+        <div>
+            <div><h1>Oi eu sou um título</h1></div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci atque fuga corporis, et dolorem, mollitia eveniet distinctio ab deserunt rem aut delectus. Quasi obcaecati, omnis in harum a quae error!</p>
+        </div>
+    )
+}
+
+export default Titulo
